f28c7213d3b36f79909632cec657fd55d042a11a
noisiak <kacper.bojko1@gmail.com>
2023-03-18 12:42:30 +0100

    Initial commit

---
+++ FiveM/Addon/nkcoquette/vehicle_names.lua
@@ -0,0 +1,35 @@
+Citizen.CreateThread(function()
+    AddTextEntry('0x7AE9116B','CallSign - 0')   -- NKCOQ_CSIGN_A0
+    AddTextEntry('0xADD1F73C','CallSign - 1')   -- NKCOQ_CSIGN_A1
+    AddTextEntry('0x2FB5ABBA','CallSign - 2')   -- NKCOQ_CSIGN_A2
+    AddTextEntry('0x168348A5','CallSign - 3')   -- NKCOQ_CSIGN_A3
+    AddTextEntry('0xC3C0A321','CallSign - 4')   -- NKCOQ_CSIGN_A4
+    AddTextEntry('0xB2E7016E','CallSign - 5')   -- NKCOQ_CSIGN_A5
+    AddTextEntry('0xE51DE5DB','CallSign - 6')   -- NKCOQ_CSIGN_A6
+    AddTextEntry('0x371B89B9','CallSign - 7')   -- NKCOQ_CSIGN_A7
+    AddTextEntry('0x68D56D2C','CallSign - 8')   -- NKCOQ_CSIGN_A8
+    AddTextEntry('0x52B040E2','CallSign - 9')   -- NKCOQ_CSIGN_A9
+
+    AddTextEntry('0x586A566C','CallSign - 0')   -- NKCOQ_CSIGN_B0
+    AddTextEntry('0x62966AC4','CallSign - 1')   -- NKCOQ_CSIGN_B1
+    AddTextEntry('0x85E7B166','CallSign - 2')   -- NKCOQ_CSIGN_B2
+    AddTextEntry('0xD82DD5F1','CallSign - 3')   -- NKCOQ_CSIGN_B3
+    AddTextEntry('0xA943781D','CallSign - 4')   -- NKCOQ_CSIGN_B4
+    AddTextEntry('0x7B979CC6','CallSign - 5')   -- NKCOQ_CSIGN_B5
+    AddTextEntry('0xC151A839','CallSign - 6')   -- NKCOQ_CSIGN_B6
+    AddTextEntry('0x13A4CCDE','CallSign - 7')   -- NKCOQ_CSIGN_B7
+    AddTextEntry('0xE6B57300','CallSign - 8')   -- NKCOQ_CSIGN_B8
+    AddTextEntry('0xB91E97D3','CallSign - 9')   -- NKCOQ_CSIGN_B9
+
+    AddTextEntry('0x82520B93','CallSign - 0')   -- NKCOQ_CSIGN_C0
+    AddTextEntry('0x928FAC0E','CallSign - 1')   -- NKCOQ_CSIGN_C1
+    AddTextEntry('0xB4CFF08E','CallSign - 2')   -- NKCOQ_CSIGN_C2
+    AddTextEntry('0x471D952B','CallSign - 3')   -- NKCOQ_CSIGN_C3
+    AddTextEntry('0x8BEF1EB5','CallSign - 4')   -- NKCOQ_CSIGN_C4
+    AddTextEntry('0x8DAA222B','CallSign - 5')   -- NKCOQ_CSIGN_C5
+    AddTextEntry('0x9D6AC1AC','CallSign - 6')   -- NKCOQ_CSIGN_C6
+    AddTextEntry('0xAF3D6551','CallSign - 7')   -- NKCOQ_CSIGN_C7
+    AddTextEntry('0x33516D77','CallSign - 8')   -- NKCOQ_CSIGN_C8
+    AddTextEntry('0xD507B0E5','CallSign - 9')   -- NKCOQ_CSIGN_C9
+
+end)
